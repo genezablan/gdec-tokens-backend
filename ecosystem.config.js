a1ce853b09e4dbda8866e3205bb68e1940db9c02
Gene Morrisey Zablan <genemorriseyzablan@gmail.com>
2026-02-19 13:22:50 +0800

    chore: split deploy workflow into staging and production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,5 +12,17 @@ module.exports = {
         PORT: 3000,
       },
     },
+    {
+      name: 'gdec-tokens-backend-staging',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M',
+      env_production: {
+        NODE_ENV: 'staging',
+        PORT: 3000,
+      },
+    },
   ],
 };
